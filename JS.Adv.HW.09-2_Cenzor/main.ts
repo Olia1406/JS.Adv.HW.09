1cc5010c5948ebd97b9a3335a103591bc503c168
Olia1406 <yurasolia@gmail.com>
2020-07-05 18:04:25 +0300

    add JS.Adv.HW.09

---
+++ JS.Adv.HW.09-2_Cenzor/main.ts
@@ -0,0 +1,70 @@
+// Завдання 5
+// Необхідно відтворити функціонал як на відео Cenzor, а саме:
+// •	При кліку на ADD добавляти заборонене слово, яке відображатиметься в списку “Bad words”
+// •	Якщо поле для добавлення слова не заповнене виводити відповідне повідомлення
+// •	При кліку на Cenzor перевіряється чи є в textarea заборонене слово, якщо так його заміняє на *, причому на ту кількість яка довжина слова
+// •	Якщо textarea порожня виводити повыдолення про заповнення поля
+
+let add = document.querySelector('.add') as HTMLButtonElement;
+let reset = document.querySelector('.reset') as HTMLButtonElement;
+let badW = document.querySelector('.badW') as HTMLDivElement;
+let inp = document.querySelector('.inp') as HTMLInputElement;
+let bwList = document.querySelector('.bwList');
+let cenzor = document.querySelector('.cenzor') as HTMLButtonElement;
+let arr: string[] = ['some'];
+let area = document.querySelector('#area') as HTMLTextAreaElement;
+// при кліку на кнопку Add створюється новий елемент зі значенням,яке було у полі, 
+// (якщо поле не пусте), потім цей елемент вставляється в кінець списку заборонених слів
+add.addEventListener('click', function (): void {
+    let h5 = document.createElement('h5');
+    if (inp.value !== '') {
+        // масив наповнюється словами з поля (потім текст буде звірятися зі словами з цього масиву)
+        arr.push(inp.value);
+        if (bwList.textContent !== '') {
+            // якщо у списку вже є слова, то додаємо ще кому перед новим словом
+            h5.textContent = `, ${inp.value}`;
+            inp.placeholder = 'write a word';
+            inp.style.borderColor = 'rgb(207, 207, 207)';
+        }
+        else {
+            // якщо список був пустий, то перед першим словом кому не додаємо
+            h5.textContent = `  ${inp.value}`;
+            inp.placeholder = 'write a word';
+            inp.style.borderColor = 'rgb(207, 207, 207)';
+        }
+    }
+    else {
+        // якщо значення у полі пусте, то виводиться повідомлення у плейсхолдер
+        inp.placeholder = 'Please write a word!';
+        inp.style.borderColor = 'red';
+    }
+    bwList.appendChild(h5);
+    inp.value = '';
+})
+// при кліку на кнопку reset список стає пустим
+reset.addEventListener('click', function () {
+    bwList.textContent = '';
+    // масив заборонених слів теж
+    arr = [];
+})
+// при кліку на кнопку cenzor:
+cenzor.addEventListener('click', function () {
+    if (area.value == '') {
+        // якщо нема ніякого тексту, то виводиться повідомлення
+        area.placeholder = 'Please write a text!';
+        area.style.borderColor = 'red';
+    }
+    else {
+        // перевіряється чи є в тексті слова з масиву заборонених
+        for (let i = 0; i < arr.length; i++) {
+            // створюється регулярний вираз з кожного слова з масиву
+            let regExp = new RegExp(`${arr[i]}`, 'gi');
+            // дане слово з масиву заміняється зірочками
+            let prohbW = `${arr[i]}`.slice(0, 0).padEnd(`${arr[i]}`.length, '*');
+            // текст заміняється на новий - з зірочками замість заборонених слів
+            area.value = area.value.replace(regExp, prohbW);
+        }
+        area.placeholder = 'text here..';
+        area.style.borderColor = 'rgb(207, 207, 207)';
+    }
+})
